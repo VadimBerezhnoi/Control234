ceda744f0913dcc0ed6a03f7d1decf3f7e6629cf
VadimBerezhnoi <tsigma66@gmail.com>
2023-04-07 18:36:44 +0300

    Написана программа сравнивающая длину элементов массива и выводящая на экран элементы массива, длина которых меньше, либо равна трем

--- Task/Control.cs
+++ Task/Control.cs
@@ -5,5 +5,26 @@ string[] secondArray = new string[firstArray.Length];
 
 void CompareLength(string[] firstArray, string[] secondArray)
 {
-    
+    int count = 0;
+    for (int i = 0; i < firstArray.Length; i++)
+    {
+        if (firstArray[i].Length <= 3)
+        {
+            secondArray[count] = firstArray[i];
+            count++;
+        }
+    }
 }
+
+
+void PrintArray (string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"{array[i] }");
+    }
+    Console.WriteLine();
+}
+
+CompareLength(firstArray, secondArray);
+PrintArray(secondArray);
