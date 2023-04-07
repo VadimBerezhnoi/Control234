39126ada6e1040a01c548a09f0c609bd652af884
VadimBerezhnoi <tsigma66@gmail.com>
2023-04-07 18:29:57 +0300

    Исправлен README

--- Task/Control.cs
+++ Task/Control.cs
@@ -1,1 +1,9 @@
-﻿
+﻿Console.Clear();
+
+string[] firstArray = new string[5] {"Kazakhstan", "lul", "563", "mom", "repeat"};
+string[] secondArray = new string[firstArray.Length];
+
+void CompareLength(string[] firstArray, string[] secondArray)
+{
+    
+}
